99be1c419ac740250789ac693d9c6637ec32e122
JanR <janrossbach3@gmail.com>
2018-03-21 08:53:04 +0100

    Lecture 8

--- groups/admin.py
+++ groups/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+
+class GroupMemberInLIne(admin.TabularInline):
+    model = models.GroupMember
+
+
+admin.sige.register(models.Group)
